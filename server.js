8a979a199709239fb5cb15aa8f5b43519e4da29b
Andrew <duskthoth@gmail.com>
2022-02-05 20:01:21 -0300

    esse aqui

--- server.js
+++ server.js
@@ -3,22 +3,23 @@ const bodyParser = require("body-parser")
 const res = require("express/lib/response")
 const req = require("express/lib/request")
 const { param } = require("express/lib/request")
+const cors = require("cors")
+const { Hub, sseHub } = require("@toverux/expresse")
 
 //Server stuff
 //========================================================================================================
-
+//calma q tem gente falando aqui
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get("/status", (request, response) =>
-  response.json({ clients: clients.length })
-)
+app.get('/status', (request, response) => response.json({clients: clients.length}));
 
-const PORT = 3000
+const PORT = 3000;
 
-let clients = []
+let clients = [];
 
 app.listen(PORT, () => {
   console.log(`Service listening at http://localhost:${PORT}/server`)
@@ -32,111 +33,64 @@ let fila2 = []
 
 //========================================================================================================
 //Event callback handling stuff
-function eventsHandler(request, response, next) {
-  const headers = {
-    "Content-Type": "text/event-stream",
-    Connection: "keep-alive",
-    "Cache-Control": "no-cache",
-  }
-  response.writeHead(200, headers)
 
-  const clientId = Date.now()
-
-  const newClient = {
-    id: clientId,
-    response,
-  }
-
-  clients.push(newClient)
-
-  request.on("close", () => {
-    console.log(`${clientId} Connection closed`)
-    clients = clients.filter((client) => client.id !== clientId)
-  })
-}
 //========================================================================================================
 // Event endpoint
-app.get("/events", eventsHandler)
-//Posting the resource on the client
-function callbackFunction(parametros) {
-    console.log('=================CALLBACK FUNCTION====================== ')
-  console.log(parametros)
-  console.log(token2)
-  if(clients.length != 0){
-  if (parametros[0].localeCompare("recurso=1") == 0) {
-    clients.forEach((client) => {
-      if (client == fila1.at(0)) {
-        client.response.write("Liberado")
-      }
-    })
-    fila1.pop()
-    token1 = null;
-  } else {
-    clients.forEach((client) => {
-        if (client == fila2.at(0)) {
-          client.response.write("Liberado")
-        }
-      })
-      fila2.pop()
-      token2 = null;
-  }
 
-}else{
-    if (parametros[0].localeCompare("recurso=1") == 0){
-        token1 = null;
-    }
-    else{
-        token2 = null;
-    }
-    console.log(token1)
-    console.log(token2)
-    console.log('ENDED')
-}}
-async function releaseToken(request, respsonse, next) {
-  console.log('=================RELEASE TOKEN====================== ')
-  var parametros = request.originalUrl.split("?")
-  parametros = parametros[1].split("&")
-  return callbackFunction(parametros)
-}
-
-app.get("/server", function (req, res) {
-  res.send("Server RUNNING  aaaaaaa!")
-})
 //========================================================================================================================
-//rest
-
+//REQUISITA TOKEN PARA ACESSO
 app.get("/server/requestToken", function (req, res) {
-  //const rec = req.parac
+  //recepe parametros pela URL
   var parametros = req.originalUrl.split("?")
   parametros = parametros[1].split("&")
+ 
 
-  console.log(parametros)
+  //Utiliza os parametros para decidir qual recurso deve ser gerado o token
   if (parametros[0].localeCompare("recurso=1") == 0) {
     if (token1 == null) {
       token1 = getRandomString(5)
-      //res.json(token);
-      res.send(token1)
-    } else {
-      fila1.parametros[1]
-    }
+      res.send(token1) //envia para o cliente
+    } else if (!fila1.includes(res)) {
+      fila1.push(res)
+    } //adiciona a fila de espera
   } else if (parametros[0].localeCompare("recurso=2") == 0) {
     if (token2 == null) {
       token2 = getRandomString(5)
-      //res.json(token);
-      res.send(token2)
-    } else {
-      fila2.push(parametros[1])
+      res.send(token2) //envia ao cliente
+    } else if (!fila2.includes(res)) {
+      fila2.push(res)
+      //adiciona o cliente a fila de espera
     }
   }
-  console.log('=================REQUEST TOKEN====================== ')
-  console.log(token1)
-  console.log(token2)
-  console.log(fila1)
-  console.log(fila2)
+  clients.push(parametros[1])
+  res.send('Finalizado')
+    
+  
+  console.log("=================REQUEST TOKEN====================== ")
+  console.log("Token1: " + token1)
+  console.log("Token2: " + token2)
+  console.log("Fila 1: " + fila1)
+  console.log("Fila 2: " + fila2)
 })
 
-app.patch("/server/releaseToken", releaseToken)
+//========================================================================================================================
+//LIBERA O TOKEN CRIADO
+app.get("/server/releaseToken", function(req,res){
+  var parametros = req.originalUrl.split("?")
+  parametros = parametros[1].split("&")
 
+  if (parametros[0].localeCompare("recurso=1") == 0) {
+    console.log(req.hostname)
+  } else if (parametros[0].localeCompare("recurso=2") == 0) {
+    console.log(req.headers.origin)
+    
+  }
+  clients.push(parametros[1])
+  res.send('Finalizado')
+})
+
+//========================================================================================================================
+//RECURSOS DO SERVIDOR, DEVOLVEM APENAS UMA STRING
 app.get("/server/resource/1", function (req, res) {
   res.send("usou recurso 1")
 })
